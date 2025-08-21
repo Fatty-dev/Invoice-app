235db361e7b7e6cd4d0b0715e0fc0c4260b5d7f6
Fatty <sholaodusiji@gmail.com>
2025-08-21 10:27:02 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 
 export default defineConfig({
+
   plugins: [react()],
-  base: '/', // ensures proper asset resolution on Vercel
-})
+
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
+});
